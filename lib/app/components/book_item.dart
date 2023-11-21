797b32eb516566f9de62ea5f8acc456af4a74817
joel felipe <joel@apetrus.com.br>
2023-11-21 14:49:57 -0300

    marcador para adicionar aos favoritos

--- lib/app/components/book_item.dart
+++ lib/app/components/book_item.dart
@@ -11,16 +11,31 @@ class BookItem extends StatefulWidget {
 }
 
 class _BookItemState extends State<BookItem> {
+
+  bool _isFavorite = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        image: DecorationImage(
-          image: NetworkImage(widget.book.coverUrl ?? ""),
-          fit: BoxFit.contain
+    return Stack(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            image: DecorationImage(
+                image: NetworkImage(widget.book.coverUrl ?? ""),
+                fit: BoxFit.contain),
+          ),
         ),
-      ),
+        Positioned(
+          top: -4,
+          right: 10,
+          child: InkWell(onTap: (){
+            setState(() {
+              _isFavorite = !_isFavorite;
+            });
+          }, child: Icon(Icons.bookmark, color:_isFavorite ? Colors.red  : Colors.amber,size: 30,)),
+        )
+      ],
     );
   }
 }
