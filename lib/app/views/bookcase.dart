34f5ce003460015c26d90ff763554f1891411e1d
joel felipe <joel@apetrus.com.br>
2023-11-21 13:36:56 -0300

    adiconando botao favoritos

--- lib/app/views/bookcase.dart
+++ lib/app/views/bookcase.dart
@@ -8,14 +8,99 @@ class BookCase extends StatefulWidget {
 }
 
 class _BookCaseState extends State<BookCase> {
+
+  bool _onlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container()
-      ],
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            _selectFavorites()
+          ],
+        ),
+      ),
     );
   }
-}
 
+  Widget _selectFavorites() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          InkWell(
+            onTap: !_onlyFavorites
+                ? null
+                : () {
+              setState(() {
+                _onlyFavorites = !_onlyFavorites;
+              });
+            },
+            child: Container(
+              height: 42,
+              width: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  bottomLeft: Radius.circular(20),
+                ),
+                border: Border.all(
+                  width: 1,
+                  color: Colors.black,
+                ),
+                color: !_onlyFavorites ? Colors.red : Colors.transparent,
+              ),
+              child: Text(
+                'Todos',
+                style: TextStyle(
+                  color:
+                  !_onlyFavorites ? Colors.white : Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: _onlyFavorites
+                ? null
+                : () {
+              setState(() {
+                _onlyFavorites = !_onlyFavorites;
+              });
+            },
+            child: Container(
+              height: 42,
+              width: 150,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(20),
+                  bottomRight: Radius.circular(20),
+                ),
+                border: Border.all(
+                  width: 1,
+                  color: Colors.black,
+                ),
+                color: _onlyFavorites ? Colors.red : Colors.transparent,
+              ),
+              child: Text(
+                'Favoritos',
+                style: TextStyle(
+                  color:
+                  _onlyFavorites ? Colors.white : Colors.black,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
+}
 
