c6db9dc1b35771e790a07e5f0c0a6600c83dda93
joel felipe <joel@apetrus.com.br>
2023-11-21 13:23:29 -0300

    base da aplicação e organização de pastas

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# estante-virtual
+# estante_virtual
+
 Desafio para vaga de desenvolvedor flutter na Escribo Inovação para o Aprendizado.
+
---
+++ lib/app/views/bookcase.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class BookCase extends StatefulWidget {
+  const BookCase({Key? key}) : super(key: key);
+
+  @override
+  State<BookCase> createState() => _BookCaseState();
+}
+
+class _BookCaseState extends State<BookCase> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Container()
+      ],
+    );
+  }
+}
+
+
